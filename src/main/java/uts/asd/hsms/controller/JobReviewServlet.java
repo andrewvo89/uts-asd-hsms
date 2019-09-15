02f7256ad9ad6fe0d9ff7fabc00658bc0281e665
Andrew Vo-Nguyen <andrew.vo89@gmail.com>
2019-09-15 21:35:50 +1000

    Updated Job Board and Job Review

---
+++ src/main/java/uts/asd/hsms/controller/JobReviewServlet.java
@@ -0,0 +1,150 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uts.asd.hsms.controller;
+
+import com.mongodb.BasicDBObject;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.mail.MessagingException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import org.bson.types.ObjectId;
+import uts.asd.hsms.model.Job;
+import uts.asd.hsms.model.JobApplication;
+import uts.asd.hsms.model.User;
+
+/**
+ *
+ * @author Andrew
+ */
+public class JobReviewServlet extends HttpServlet {
+    private ObjectId jobAppliationId, jobId, userId;
+    private BasicDBObject status;
+    private JobReviewController controller;
+    private HttpServletRequest request;
+    private HttpServletResponse response;
+    private JobApplication jobApplication;
+    private Job job;
+    private User user;
+    private HttpSession session;
+    private ArrayList<String> message;
+    private Date zeroDate = new Date();
+    private EmailNotifier emailNotifier;
+    private String statusEmail;
+    
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {        
+    }    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        this.response = response;
+        this.request = request;    
+        session = request.getSession();
+        message = new ArrayList<>();
+        controller = new JobReviewController(session);
+        String action = request.getParameter("action");
+        statusEmail = ""; 
+        jobAppliationId = new ObjectId(request.getParameter("jobApplicationId"));
+        zeroDate.setTime(0);
+        message.add("Status Change Result");
+        jobApplication = null;
+        Boolean editSucess = false;
+        emailNotifier = new EmailNotifier();
+        
+        if (controller.getJobApplications(jobAppliationId, null, null, null, null, "jobid", 1).length != 0) {//If exists in database
+            jobApplication = controller.getJobApplications(jobAppliationId, null, null, null, null, "jobid", 1)[0];
+            jobId = jobApplication.getJobId();
+            userId = jobApplication.getUserId();
+            status = jobApplication.getStatus();
+            job = controller.getJobs(jobId, null, null, null, null, null, null, null, "_id", 1)[0];
+            user = controller.getUsers(userId, null, null, null, null, null, null, null, 0, "_id", 1)[0];
+            //Depending on which button got pressed on the previous page
+            switch (action) {
+                case "review":
+                    reviewJobApplication();
+                    break;
+                case "reject":
+                    rejectJobApplication();
+                    break;
+                case "success":
+                    successfulJobApplication();
+                    break;
+                default:
+                    break;
+            }
+            jobApplication.setStatus(status);        
+            if (controller.editJobApplication(jobApplication)) editSucess = true;
+        }        
+        if (editSucess) {
+            try {
+                sendEmail(user.getFirstName(), job.getTitle(), statusEmail);
+                message.add(String.format("%s's Job Application has now been updated. A notification email has been sent to %s", user.getFirstName(), user.getEmail()));
+            } catch (MessagingException ex) {
+                message.add(String.format("%s's Job Application has now been updated. A notification email has <b>failed</b> to be sent to %s", user.getFirstName(), user.getEmail()));
+            }
+            message.add("success");
+        }
+        else {
+            message.add(String.format("Status update failed"));
+            message.add("danger");
+        }
+        session.setAttribute("message", message);
+        response.sendRedirect("jobreview.jsp?jobId=" + jobId.toString());
+    }
+    
+    public void reviewJobApplication() throws ServletException, IOException {
+        //All following step's dates should be changed back to 0
+        status.put("review", new Date()); status.put("rejected", zeroDate); status.put("successful", zeroDate);
+        statusEmail = "Under Review";
+    }
+    public void rejectJobApplication() throws ServletException, IOException {
+        //Success date should be changed back to 0
+        status.put("rejected", new Date()); status.put("successful", zeroDate);
+        statusEmail = "Rejected";
+    }
+    public void successfulJobApplication()throws ServletException, IOException {
+        //Write current sucess date to current date
+        status.put("successful", new Date()); 
+        statusEmail = "Successful";
+        //Write rejected status to today on all other applicants & write success on any other applicant to 0
+        JobApplication[] jobApplications = controller.getJobApplications(null, jobId, null, null, null, "_id", 1);
+        for (JobApplication rejecetedJobApplication : jobApplications) {
+            if (!rejecetedJobApplication.getUserId().equals(userId)) {
+                //Get rejected User
+                User rejectedUser = controller.getUsers(rejecetedJobApplication.getUserId(), null, null, null, null, null, null, null, 0, "_id", 1)[0];
+                //Edit all other users that's not current user in question
+                BasicDBObject tempStatus = rejecetedJobApplication.getStatus();
+                tempStatus.put("rejected", new Date());
+                tempStatus.put("successful", zeroDate);
+                rejecetedJobApplication.setStatus(tempStatus);
+                controller.editJobApplication(rejecetedJobApplication);
+                try {
+                    //Send rejected email to all other applicants
+                    sendEmail(rejectedUser.getFirstName(), job.getTitle(), "Rejected");
+                } catch (MessagingException ex) {
+                    Logger.getLogger(JobReviewServlet.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }    
+    }
+    public void sendEmail(String firstName, String jobTitle, String jobApplicationStatus) throws MessagingException {
+        emailNotifier.sendEmail("uts.asd.hsms@gmail.com", "Job Application: " + jobTitle, String.format("Dear %s,\n\n"
+        + "Your job application for the %s role has been updated to: %s.\n"
+        + "To check your current Job Application's status, visit https://uts-asd-hsms.herokuapp.com/jobboard.jsp\n\n"              
+        + "Kind Regards,\nThe HSMS Team", firstName, jobTitle, jobApplicationStatus));
+    }
+}
---
+++ target/hsms-1.0-SNAPSHOT/js/jobreview
@@ -0,0 +1,90 @@
+//Upon page loading
+$(window).on('load', function () {
+    var modalTrigger = document.getElementById('modalTrigger').value;
+    if (modalTrigger != '') {
+        $('#messageModal').modal('show');
+    }
+    checkWorkType();
+    checkDepartment();
+});
+
+$('input[name=workTypeSearch]').on('change', function () {
+    checkWorkType();
+});
+$('input[name=departmentSearch]').on('change', function () {
+    checkDepartment();
+});
+
+//Change Pill button status based on Radio Button selected
+function checkWorkType() {
+    $("#workTypeButtonFullTime").removeClass('active');
+    $("#workTypeButtonPartTime").removeClass('active');
+    $("#workTypeButtonCasual").removeClass('active');
+    $("#workTypeButtonContract").removeClass('active');
+    if ($('input[id=workTypeSearchFullTime]').prop('checked')) {
+        $("#workTypeButtonFullTime").addClass('active');
+    } else if ($('input[id=workTypeSearchPartTime]').prop('checked')) {
+        $("#workTypeButtonPartTime").addClass('active');
+    } else if ($('input[id=workTypeSearchCasual]').prop('checked')) {
+        $("#workTypeButtonCasual").addClass('active');
+    } else if ($('input[id=workTypeSearchContract]').prop('checked')) {
+        $("#workTypeButtonContract").addClass('active');
+    }
+}
+function checkDepartment() {
+    $("#departmentButtonAdministration").removeClass('active');
+    $("#departmentButtonEnglish").removeClass('active');
+    $("#departmentButtonMath").removeClass('active');
+    $("#departmentButtonScience").removeClass('active');
+    $("#departmentButtonArt").removeClass('active');
+    if ($('input[id=departmentSearchAdministration]').prop('checked')) {
+        $("#departmentButtonAdministration").addClass('active');
+    } else if ($('input[id=departmentSearchEnglish]').prop('checked')) {
+        $("#departmentButtonEnglish").addClass('active');
+    } else if ($('input[id=departmentSearchMath]').prop('checked')) {
+        $("#departmentButtonMath").addClass('active');
+    } else if ($('input[id=departmentSearchScience]').prop('checked')) {
+        $("#departmentButtonScience").addClass('active');
+    } else if ($('input[id=departmentSearchArt]').prop('checked')) {
+        $("#departmentButtonArt").addClass('active');
+    }
+}
+//Work Type Button Actions
+$("#workTypeButtonFullTime").on('click', function () {
+    document.getElementById('workTypeSearchFullTime').checked = true;
+    document.getElementById('searchButton').click();
+});
+$("#workTypeButtonPartTime").on('click', function () {
+    document.getElementById('workTypeSearchPartTime').checked = true;
+    document.getElementById('searchButton').click();
+});
+$("#workTypeButtonCasual").on('click', function () {
+    document.getElementById('workTypeSearchCasual').checked = true;
+    document.getElementById('searchButton').click();
+});
+$("#workTypeButtonContract").on('click', function () {
+    document.getElementById('workTypeSearchContract').checked = true;
+    document.getElementById('searchButton').click();
+});
+
+//Department Button Actions
+$("#departmentButtonAdministration").on('click', function () {
+    document.getElementById('departmentSearchAdministration').checked = true;
+    document.getElementById('searchButton').click();
+});
+$("#departmentButtonEnglish").on('click', function () {
+    document.getElementById('departmentSearchEnglish').checked = true;
+    document.getElementById('searchButton').click();
+});
+$("#departmentButtonMath").on('click', function () {
+    document.getElementById('departmentSearchMath').checked = true;
+    document.getElementById('searchButton').click();
+});
+$("#departmentButtonScience").on('click', function () {
+    document.getElementById('departmentSearchScience').checked = true;
+    document.getElementById('searchButton').click();
+});
+$("#departmentButtonArt").on('click', function () {
+    document.getElementById('departmentSearchArt').checked = true;
+    document.getElementById('searchButton').click();
+});
